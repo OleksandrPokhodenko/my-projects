02b58c3fb4257627c39a902696ee868c8aab4a48
OleksandrPokhodenko <160655734+OleksandrPokhodenko@users.noreply.github.com>
2025-03-03 00:24:15 +0200

    Add files via upload

--- photosnap/js/script.js
+++ photosnap/js/script.js
@@ -32,3 +32,71 @@ const resizeObserver = new ResizeObserver(entries => {
 })
 
 resizeObserver.observe(document.documentElement)
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttonChoice = document.querySelector('.box-button__button')
+    const buttonChoices = document.querySelectorAll('.box-button__choice')
+    const priceElements = document.querySelectorAll('.box-articles__body')
+
+    const tariffPlansObject = {
+        Basic: {
+            Monthly: '$19.00',
+            Yearly: '$190.00',
+        },
+        Pro: {
+            Monthly: '$39.00',
+            Yearly: '$390.00',
+        },
+        Business: {
+            Monthly: '$99.00',
+            Yearly: '$990.00',
+        },
+    }
+
+    let plan = localStorage.getItem('plan') || 'Monthly'
+
+    function updatePricing() {
+        priceElements.forEach(article => {
+            const planName = article.getAttribute('data-plan')
+            const planValue = article.querySelector('.box-articles__price')
+            const planTariff = article.querySelector('.box-articles__pricing-plan')
+
+            if (tariffPlansObject[planName]) {
+                planValue.classList.add('fade-out')
+                planTariff.classList.add('fade-out')
+
+                setTimeout(() => {
+                    planValue.textContent = tariffPlansObject[planName][plan]
+                    planTariff.textContent = plan === 'Monthly' ? 'per month' : 'per year'
+
+                    planValue.classList.remove('fade-out')
+                    planTariff.classList.remove('fade-out')
+
+                    planValue.classList.add('fade-in')
+                    planTariff.classList.add('fade-in')
+                }, 200)
+            }
+        })
+    }
+
+    if (buttonChoice && buttonChoices.length > 0) {
+        buttonChoice.addEventListener('click', () => {
+            buttonChoice.classList.toggle('box-button__button--right')
+
+            buttonChoices.forEach(choice => choice.classList.toggle('active'))
+
+            const activeChoice = document.querySelector('.box-button__choice.active')
+            plan = activeChoice ? activeChoice.innerText : 'Monthly'
+            updatePricing()
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
